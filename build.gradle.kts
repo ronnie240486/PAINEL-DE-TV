a0512ec7e46e0452a29b338bd6be5513b0150308
ronnie240486 <ronnie240486@gmail.com>
2025-09-08 22:36:07 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
+val ktor_version = "2.3.10"
+val kotlin_version = "1.9.23"
+val logback_version = "1.2.11"  // Ou qualquer versão que você desejar
 
 plugins {
     kotlin("jvm") version "1.9.23"
@@ -12,7 +12,7 @@ group = "com.example"
 version = "0.0.1"
 
 application {
-    mainClass.set("com.example.ApplicationKt")
+    mainClass.set("com.example.ApplicationKt") // A classe principal do seu app Ktor
 }
 
 repositories {
@@ -21,21 +21,21 @@ repositories {
 
 dependencies {
     // Ktor Core
-    implementation("io.ktor:ktor-server-core-jvm")
-    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
 
     // Content Negotiation for JSON
-    implementation("io.ktor:ktor-server-content-negotiation-jvm")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
 
     // Logging
-    implementation("io.ktor:ktor-server-call-logging-jvm")
+    implementation("io.ktor:ktor-server-call-logging-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     
-    // Coroutines (already included with Kotlin, but good practice to be explicit)
+    // Coroutines (já incluído com Kotlin, mas é bom ser explícito)
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
 
     // Testing
-    testImplementation("io.ktor:ktor-server-tests-jvm")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
